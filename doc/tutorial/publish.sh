8d2bf6cae66b937deca21e215eb4a3638356c8a1
Jomi F. Hubner <jomifred@gmail.com>
2016-04-29 16:41:50 +0200

    add some doc

---
+++ doc/tutorial/publish.sh
@@ -0,0 +1,1 @@
+scp tutorial.pdf jomifred,moise@web.sf.net:/home/groups/m/mo/moise/htdocs/doc
---
+++ src/site/create-related-papers.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export BIBINPUTS=$HOME/pubs/bib:
+
+rm *aux *dvi *log *toc *bbl *blg *~ *.backup
+
+for f in `ls y*tex`; do
+  n=`echo $f | cut -d . -f 1`
+  echo generating $n
+  latex --interaction nonstopmode $n > /dev/null
+  bibtex $n > /dev/null
+  #latex --interaction nonstopmode $n > /dev/null
+done  
+
+
+latex --interaction nonstopmode related-papers.tex > /dev/null
+latex --interaction nonstopmode related-papers.tex > out
+
+echo "generating page (see out file for the result)"
+
+#latex2html -split 0 -white -image_type=gif -transparent -math -no_navigation publicationsWebPage.tex >> out
+
+latex2html -split 0 -white -transparent -math -no_navigation related-papers.tex >> out
+
+cp related-papers/related-papers.html .
+
+#dvipdfm publicationsWebPage
+
+rm *aux *dvi *log *toc *bbl *blg *.out
+
