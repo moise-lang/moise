8f3420d8dc3bab151c0c84b3d8d6ca68fc35e8b6
Jomi F. Hubner <jomifred@gmail.com>
2017-08-17 09:44:53 -0300

    better msg error for create group/scheme

--- src/main/java/ora4mas/nopl/OrgBoard.java
+++ src/main/java/ora4mas/nopl/OrgBoard.java
@@ -1,6 +1,5 @@
 package ora4mas.nopl;
 
-import java.io.File;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.HashMap;
@@ -22,7 +21,6 @@ import cartago.OPERATION;
 import cartago.OpFeedbackParam;
 import cartago.OperationException;
 import jason.asSyntax.Atom;
-import jason.runtime.SourcePath;
 import jason.util.Config;
 import moise.common.MoiseException;
 import moise.os.OS;
@@ -97,10 +95,16 @@ public class OrgBoard extends Artifact {
     }
 
     @OPERATION public void createGroup(String id, String type, OpFeedbackParam<ArtifactId> gaid) throws OperationException {
-        ArtifactId aid = makeArtifact(id, GroupBoard.class.getName(), new ArtifactConfig(osFile, type) );
-        aids.put(id, aid);
-        defineObsProperty("group", new Atom(id), new Atom(type), aid);
-        gaid.set(aid);
+        ArtifactId aid;
+        try {
+            aid = lookupArtifact(id);
+            failed("Artifact with id "+id+" already exists!");
+        } catch (OperationException e) {
+        	aid = makeArtifact(id, GroupBoard.class.getName(), new ArtifactConfig(osFile, type) );
+	        aids.put(id, aid);
+	        defineObsProperty("group", new Atom(id), new Atom(type), aid);
+	        gaid.set(aid);
+        }
     }
 
     @OPERATION public void removeGroup(String id) {
@@ -118,10 +122,16 @@ public class OrgBoard extends Artifact {
     }
 
     @OPERATION public void createScheme(String id, String type, OpFeedbackParam<ArtifactId> said) throws OperationException {
-        ArtifactId aid = makeArtifact(id, SchemeBoard.class.getName(), new ArtifactConfig(osFile, type) );
-        aids.put(id, aid);
-        defineObsProperty("scheme", new Atom(id), new Atom(type), aid);
-        said.set(aid);
+        ArtifactId aid;
+        try {
+            aid = lookupArtifact(id);
+            failed("Artifact with id "+id+" already exists!");
+        } catch (OperationException e) {
+            aid = makeArtifact(id, SchemeBoard.class.getName(), new ArtifactConfig(osFile, type) );
+            aids.put(id, aid);
+            defineObsProperty("scheme", new Atom(id), new Atom(type), aid);
+            said.set(aid);
+        }
     }
 
     @OPERATION public void removeScheme(String id) {
